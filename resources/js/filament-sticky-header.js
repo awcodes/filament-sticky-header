9917fea31f27711708db39fd5239f6d87468d66f
awcodes <adam@adamwgd.com>
2022-07-06 14:41:40 -0400

    fix presticking issue

--- resources/js/filament-sticky-header.js
+++ resources/js/filament-sticky-header.js
@@ -1,22 +1,24 @@
 const el = document.querySelector(".filament-main-topbar");
 
 if (el) {
-    const observer = new IntersectionObserver(
-        ([e]) => {
-            if (e.isIntersecting) {
-                document.querySelector(".filament-header").removeAttribute("stuck");
-                document.querySelector(".filament-body").removeAttribute("stuck");
-            } else {
-                document
-                    .querySelector(".filament-header")
-                    .setAttribute("stuck", "true");
-                document
-                    .querySelector(".filament-body")
-                    .setAttribute("stuck", "true");
-            }
-        },
-        { threshold: [0] }
-    );
+    window.addEventListener('load', function() {
+        const observer = new IntersectionObserver(
+            ([e]) => {
+                if (e.isIntersecting) {
+                    document.querySelector(".filament-header").removeAttribute("stuck");
+                    document.querySelector(".filament-body").removeAttribute("stuck");
+                } else {
+                    document
+                        .querySelector(".filament-header")
+                        .setAttribute("stuck", "true");
+                    document
+                        .querySelector(".filament-body")
+                        .setAttribute("stuck", "true");
+                }
+            },
+            { threshold: [0] }
+        );
 
-    observer.observe(el);
+        observer.observe(el);
+    });
 }
