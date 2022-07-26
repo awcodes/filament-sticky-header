4499386e4b3f94e1c70b006e30270d117f816061
awcodes <adam@adamwgd.com>
2022-07-26 14:13:50 -0400

    modifiy scripts and styles for better transitioning

--- resources/js/plugin.js
+++ resources/js/plugin.js
@@ -1,24 +1,24 @@
 const el = document.querySelector(".filament-main-topbar");
 
 if (el) {
-    window.addEventListener('load', function() {
-        const observer = new IntersectionObserver(
-            ([e]) => {
-                if (e.isIntersecting) {
-                    document.querySelector(".filament-header").removeAttribute("stuck");
-                    document.querySelector(".filament-body").removeAttribute("stuck");
-                } else {
-                    document
-                        .querySelector(".filament-header")
-                        .setAttribute("stuck", "true");
-                    document
-                        .querySelector(".filament-body")
-                        .setAttribute("stuck", "true");
-                }
-            },
-            { threshold: [0] }
-        );
+  window.addEventListener("load", function () {
+    const observer = new IntersectionObserver(
+      ([e]) => {
+        if (e.isIntersecting) {
+          document.querySelector(".filament-header").removeAttribute("stuck");
+          document.querySelector(".filament-body").removeAttribute("stuck");
+        } else {
+          document.querySelector(".filament-header").setAttribute("stuck", "true");
+          document.querySelector(".filament-body").setAttribute("stuck", "true");
+        }
+      },
+      {
+        root: null,
+        rootMargin: `${el.offsetHeight * -1}px`,
+        threshold: [0],
+      }
+    );
 
-        observer.observe(el);
-    });
+    observer.observe(el);
+  });
 }
