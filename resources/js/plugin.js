d67f6b7dc8eba8716331906cfdc4723382750f47
Adam Weston <adam@awcodes.com>
2023-12-16 09:36:55 -0500

    Fix: restict usage to only panels using plugin class

--- resources/js/plugin.js
+++ resources/js/plugin.js
@@ -3,46 +3,48 @@ const filamentMainContent = document.querySelector(".fi-main");
 const filamentHeader = document.querySelector(".fi-header");
 
 if (filamentTopbar && filamentMainContent && filamentHeader) {
-    window.addEventListener("load", function () {
-        const trigger = document.createElement("div");
-        const theme = filamentData?.stickyHeaderTheme || 'default';
-        trigger.classList.add("filament-sticky-trigger");
-        filamentMainContent.prepend(trigger);
+    if (filamentData?.stickyHeaderActive) {
+        window.addEventListener("load", function () {
+            const trigger = document.createElement("div");
+            const theme = filamentData?.stickyHeaderTheme || 'default';
+            trigger.classList.add("filament-sticky-trigger");
+            filamentMainContent.prepend(trigger);
 
-        filamentMainContent.classList.add(`sticky-theme-${theme}`);
+            filamentMainContent.classList.add(`sticky-theme-${theme}`);
 
-        let offsetHeight = filamentTopbar.offsetHeight;
+            let offsetHeight = filamentTopbar.offsetHeight;
 
-        let intersectingTime = null;
+            let intersectingTime = null;
 
-        const observer = new IntersectionObserver(
-            ([e]) => {
+            const observer = new IntersectionObserver(
+                ([e]) => {
 
-                if (e.isIntersecting) {
-                    if (intersectingTime && (e.time - intersectingTime) < 1000) {
+                    if (e.isIntersecting) {
+                        if (intersectingTime && (e.time - intersectingTime) < 1000) {
+                            return;
+                        }
+                        intersectingTime = e.time;
+                        filamentMainContent.classList.remove("is-sticky");
                         return;
                     }
-                    intersectingTime = e.time;
-                    filamentMainContent.classList.remove("is-sticky");
-                    return;
+
+                    let offsetModifier = 0;
+
+                    if (theme.includes('floating')) {
+                        offsetModifier += 8;
+                    }
+
+                    filamentHeader.style.top = (offsetHeight + offsetModifier) + "px";
+                    filamentHeader.setAttribute("wire:ignore.self", "true");
+                    filamentMainContent.classList.add("is-sticky");
+                },
+                {
+                    rootMargin: `-${offsetHeight}px`,
+                    threshold: [0],
                 }
+            );
 
-                let offsetModifier = 0;
-
-                if (theme.includes('floating')) {
-                    offsetModifier += 8;
-                }
-
-                filamentHeader.style.top = (offsetHeight + offsetModifier) + "px";
-                filamentHeader.setAttribute("wire:ignore.self", "true");
-                filamentMainContent.classList.add("is-sticky");
-            },
-            {
-                rootMargin: `-${offsetHeight}px`,
-                threshold: [0],
-            }
-        );
-
-        observer.observe(trigger);
-    });
+            observer.observe(trigger);
+        });
+    }
 }
