1dc499ba4088af2174f8d5edb8d39d08ed4eafae
Adam Weston <adam@awcodes.com>
2025-05-29 15:26:33 -0400

    Feat: support disable on list pages

--- src/StickyHeaderPlugin.php
+++ src/StickyHeaderPlugin.php
@@ -5,10 +5,9 @@ namespace Awcodes\FilamentStickyHeader;
 use Closure;
 use Filament\Contracts\Plugin;
 use Filament\Panel;
-use Filament\Support\Assets\Css;
-use Filament\Support\Assets\Js;
 use Filament\Support\Concerns\EvaluatesClosures;
 use Filament\Support\Facades\FilamentAsset;
+use Illuminate\Support\Facades\App;
 
 class StickyHeaderPlugin implements Plugin
 {
@@ -18,11 +17,13 @@ class StickyHeaderPlugin implements Plugin
 
     protected bool | Closure | null $isFloating = null;
 
+    protected bool | Closure | null $stickOnListPages = null;
+
     public function boot(Panel $panel): void
     {
         FilamentAsset::registerScriptData([
             'stickyHeaderTheme' => $this->getTheme(),
-            'stickyHeaderActive' => true,
+            'stickyHeaderActive' => $this->shouldStick(),
         ], 'awcodes-sticky-header');
     }
 
@@ -40,9 +41,16 @@ class StickyHeaderPlugin implements Plugin
         return $this;
     }
 
-    public static function get(): static
+    public function stickOnListPages(bool | Closure $condition = true): static
     {
-        return filament(app(static::class)->getId());
+        $this->stickOnListPages = $condition;
+
+        return $this;
+    }
+
+    public static function get(): Plugin
+    {
+        return filament(App::make(static::class)->getId());
     }
 
     public function getId(): string
@@ -76,11 +84,28 @@ class StickyHeaderPlugin implements Plugin
 
     public static function make(): static
     {
-        return app(static::class);
+        return App::make(static::class);
     }
 
     public function register(Panel $panel): void
     {
         //
+    }
+
+    public function shouldStickOnListPages(): bool
+    {
+        return $this->evaluate($this->stickOnListPages) ?? true;
+    }
+
+    public function shouldStick(): bool
+    {
+        if (
+            str(request()->route()->getAction('as'))->contains('index')
+            && ! $this->shouldStickOnListPages()
+        ) {
+            return false;
+        }
+
+        return true;
     }
 }
